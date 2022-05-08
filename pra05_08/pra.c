4cf03bbfb42150035e5630a014c2a02d78ae5499
sweet <2480574683@qq.com>
2022-05-08 15:49:13 +0800

    基本分支语句

---
+++ pra05_08/pra.c
@@ -0,0 +1,97 @@
+/*
+#include <stdio.h>
+int main()
+{
+int a = 1,b = 2;
+if(a<b)
+{
+b = a;
+}
+printf("a=%d,b=%d\n",a,b);
+return 0;
+}*/
+/*
+#include <stdio.h>
+int main()
+{
+int a = 2,b =1;
+if(a<b)
+{
+b = a;
+}
+else
+{
+b= a;
+}
+printf("a=%d,b=%d\n",a,b);
+return 0;
+}*/
+/*
+#include <stdio.h>
+ 
+int main() {
+   int a, b, c;
+ 
+   a = 11;
+   b = 22;
+   c = 33;
+ 
+   if ( a > b && a > c )
+      printf("%d 最大", a);
+   else if ( b > a && b > c )
+      printf("%d 最大", b);
+   else if ( c > a && c > b )
+      printf("%d 最大", c);
+   else   
+      printf("有两个或三个数值相等");
+ 
+   return 0;
+}
+*/
+/*
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    switch(i)
+    {
+    case 1:
+    printf("case 1\n");
+    case 2:
+    printf("case 2\n");
+    case 3:
+    printf("case 3\n");
+    default:
+    printf("default\n");
+    }
+    return 0;
+}
+*/
+/*
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    switch(i)
+    {
+    case 1:
+    printf("case 1\n");
+    break;
+    case 2:
+    printf("case 2\n");
+    break;
+    case 3:
+    printf("case 3\n");
+    break;
+    default:
+    printf("default\n");
+    break;
+    }
+    return 0;
+}
+
+  */
